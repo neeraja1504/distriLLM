37dbc2f1fee6d2ba402b976a90f9ee15852875eb
neeraja1504 <43319846+neeraja1504@users.noreply.github.com>
2023-06-20 18:27:59 +0530

    replaced randn with rand

--- dataset.py
+++ dataset.py
@@ -4,18 +4,25 @@ s = np.random.default_rng().normal(mu, sigma, 10)
 print(s)
 
 mega_list=[]
-mu_list=[]
-sigma_list=[]
+mu_sigma_list=[]
 list1=[]
+pos=[]
+sequence=[]
 for i in range(0,50):
-  mu = np.random.randn()*10
-  sigma = abs(np.random.randn()*10)
+  mu = np.random.rand()*10
+  sigma = abs(np.random.rand()*10)
   for j in range(0,140):
     list1 = np.random.default_rng().normal(mu, sigma, 128)
     mega_list.append(list1)
-    mu_list.append(mu)
-    sigma_list.append(sigma)
+    temp=[mu,sigma]
+    mu_sigma_list.append(temp)
+for i in range(128):
+  pos.append(i/128)
+for i in mega_list:
+  temp=[i,pos]
+  sequence.append(temp)
     
-print(mu_list)
-print(sigma_list)
+    
+
 print(mega_list)
+print(mu_sigma_list)
