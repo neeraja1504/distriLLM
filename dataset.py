442b871ee2cc8a81eb99c67a037b2571746f85e9
neeraja1504 <43319846+neeraja1504@users.noreply.github.com>
2023-06-20 18:40:26 +0530

    added train,test,val

--- dataset.py
+++ dataset.py
@@ -13,6 +13,7 @@ for i in range(0,50):
   sigma = abs(np.random.rand()*10)
   for j in range(0,140):
     list1 = np.random.default_rng().normal(mu, sigma, 128)
+    list1.sort()
     mega_list.append(list1)
     temp=[mu,sigma]
     mu_sigma_list.append(temp)
@@ -21,7 +22,22 @@ for i in range(128):
 for i in mega_list:
   temp=[i,pos]
   sequence.append(temp)
-    
+
+sequence_train=[]
+sequence_val=[]
+sequence_test=[]
+mu_sigma_train=[]
+mu_sigma_val=[]
+mu_sigma_test=[]
+
+
+sequence_train=sequence[:5000]
+sequence_val=sequence[5000:6000]
+sequence_test=sequence[6000:7000]
+
+mu_sigma_train=mu_sigma_list[:5000]
+mu_sigma_val=mu_sigma_list[5000:6000]
+mu_sigma_test=mu_sigma_list[6000:7000]
     
 
 print(mega_list)
